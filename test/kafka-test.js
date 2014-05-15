ad8498c83d0313b2c66b0e7597647be08925cc10
Jamil Bou Kheir <jamil@elbii.com>
2014-05-15 14:21:33 -0700

    commit kafka changes so far
    kanban #1362867

--- test/kafka-test.js
+++ test/kafka-test.js
@@ -1,5 +1,6 @@
 var assert = require('chai').assert
   , sinon = require('sinon')
+  , env = process.env.NODE_ENV || 'development'
   , kafka = require('../lib/modules/kafka');
 
 describe('kafka', function () {
@@ -12,4 +13,30 @@ describe('kafka', function () {
     assert(typeof kafka.register, 'function');
     done();
   });
+
+  // TODO: stub for travis-ci.org
+  if ('development' === env) {
+    describe('topics', function () {
+      var topic = 'test'
+        , message = '{ "hello": "world" }'
+        , consumed;
+
+      // register topic first
+      before(function (next) {
+        console.log('before');
+        kafka.register(topic, function (data) {
+          console.log('data: ' + data);
+        });
+        next();
+      });
+
+      it('reads from topic', function (done) {
+        kafka.read(topic, function (message) {
+          assert.ok(message, 'message received');
+          console.log(message);
+          done();
+        });
+      });
+    });
+  }
 });
