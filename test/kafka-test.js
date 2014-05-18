d165315d38d8a57fe617467a406e0b5091d86490
Jamil Bou Kheir <jamil@elbii.com>
2014-05-18 11:41:52 -0700

    kafka is finally working

--- test/kafka-test.js
+++ test/kafka-test.js
@@ -1,39 +1,30 @@
 var assert = require('chai').assert
-  , sinon = require('sinon')
+  , Chance = require('chance')
   , env = process.env.NODE_ENV || 'development'
   , kafka = require('../lib/modules/kafka');
 
 describe('kafka', function () {
-  it ('ensures read is a function', function (done) {
-    assert(typeof kafka.read, 'function');
-    done();
-  });
-
-  it('ensures register is a function', function (done) {
-    assert(typeof kafka.register, 'function');
-    done();
-  });
-
   // TODO: stub for travis-ci.org
-  if ('development' === env) {
+  if (!process.env.IN_TRAVIS) {
     describe('topics', function () {
-      var topic = 'test'
-        , message = '{ "hello": "world" }'
+      var topic = 'opensoc-test'
+        , chance = new Chance()
+        , message = chance.paragraph()
         , consumed;
 
       // register topic first
       before(function (next) {
-        console.log('before');
-        kafka.register(topic, function (data) {
-          console.log('data: ' + data);
-        });
-        next();
+        kafka.register(topic, next);
+      });
+
+      before(function (next) {
+        kafka.produce(topic, message, next);
       });
 
       it('reads from topic', function (done) {
-        kafka.read(topic, function (message) {
-          assert.ok(message, 'message received');
-          console.log(message);
+        kafka.consume(topic, function (received) {
+          assert.ok(received, 'message received');
+          assert.equal(received.value, message);
           done();
         });
       });
