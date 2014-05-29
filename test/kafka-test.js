756bf2aa3c1ab8cd67ac8e501a0ec43b7f30be0f
Jamil Bou Kheir <jamil@elbii.com>
2014-05-29 16:27:59 -0700

    don't require kafka if running in travis

--- test/kafka-test.js
+++ test/kafka-test.js
@@ -1,37 +1,37 @@
 /*global assert: true*/
 
+if (!process.env.IN_TRAVIS) {
 var assert = require('chai').assert
   , Chance = require('chance')
   , env = process.env.NODE_ENV || 'dev'
   , kafka = require('../lib/modules/kafka');
 
 describe('kafka', function () {
-  if (!process.env.IN_TRAVIS) {
-    describe('topics', function () {
-      var chance = new Chance()
-        , message = chance.paragraph()
-        , topic = 'test-' + chance.word()
-        , consumed;
+  describe('topics', function () {
+    var chance = new Chance()
+      , message = chance.paragraph()
+      , topic = 'test-' + chance.word()
+      , consumed;
 
-      // register topic first
-      before(function (next) {
-        kafka.register(topic, next);
-      });
+    // register topic first
+    before(function (next) {
+      kafka.register(topic, next);
+    });
 
-      before(function (next) {
-        // wait a bit for kafka to determine topic leader
-        setTimeout(function () {
-          kafka.produce(topic, message, next);
-        }, 1000);
-      });
+    before(function (next) {
+      // wait a bit for kafka to determine topic leader
+      setTimeout(function () {
+        kafka.produce(topic, message, next);
+      }, 1000);
+    });
 
-      it('reads from topic', function (done) {
-        kafka.consume(topic, function (received) {
-          assert.ok(received, 'message received');
-          assert.equal(received.value, message);
-          done();
-        });
+    it('reads from topic', function (done) {
+      kafka.consume(topic, function (received) {
+        assert.ok(received, 'message received');
+        assert.equal(received.value, message);
+        done();
       });
     });
-  }
+  });
 });
+}
