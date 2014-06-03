3fbdeda135d698d4355ca3c661c1426239e69d80
Jamil Bou Kheir <jamil@elbii.com>
2014-06-03 16:24:26 -0700

    test session cookie set / unset
    OSP-16

--- test/session-test.js
+++ test/session-test.js
@@ -4,40 +4,48 @@ var assert = require('chai').assert
   , app = require('../lib/opensoc-ui').app;
 
 describe('sessions', function () {
-  var session = request.agent(app);
+  describe('log in / log out', function () {
+    var session = request.agent(app);
 
-  it('logs in', function (done) {
-    session.
-      post('/login').
-      send({ email: 'joesmith@opensoc.dev', password: 'opensoc' }).
-      end(function (err, res) {
-        // redirects to home
-        assert.equal(res.header['location'], '/');
-        assert.equal(res.statusCode, 302);
-        done();
-      });
+    it('logs in', function (done) {
+      session.
+        post('/login').
+        send({ email: 'joesmith@opensoc.dev', password: 'opensoc' }).
+        end(function (err, res) {
+          // redirects to home
+          assert.equal(res.header['location'], '/');
+          assert.match(res.header['set-cookie'], /joesmith/);
+          assert.equal(res.statusCode, 302);
+          done();
+        });
+    });
+
+    it('logs out', function (done) {
+      session.
+        get('/logout').
+        end(function (err, res) {
+          // redirects to login
+          assert.equal(res.header['location'], '/login');
+          assert.notMatch(res.header['set-cookie'], /joesmith/);
+          assert.equal(res.statusCode, 302);
+          done();
+        });
+    });
   });
 
-  it('fails log in', function (done) {
-    session.
-      post('/login').
-      send({ email: 'joesmith@opensoc.dev', password: 'foobar' }).
-      end(function (err, res) {
-      console.log(res);
-        assert.equal(res.header['location'], '/login');
-        assert.equal(res.statusCode, 302);
-        done();
-      });
-  });
+  describe('session not set', function () {
+    var session = request.agent(app);
 
-  it('logs out', function (done) {
-    session.
-      get('/logout').
-      end(function (err, res) {
-        // redirects to login
-        assert.equal(res.header['location'], '/login');
-        assert.equal(res.statusCode, 302);
-        done();
-      });
+    it('fails log in', function (done) {
+      session.
+        post('/login').
+        send({ email: 'joesmith@opensoc.dev', password: 'foobar' }).
+        end(function (err, res) {
+          assert.equal(res.header['location'], '/login');
+          assert.notMatch(res.header['set-cookie'], /joesmith/);
+          assert.equal(res.statusCode, 302);
+          done();
+        });
+    });
   });
 });
