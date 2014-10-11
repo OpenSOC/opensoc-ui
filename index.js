a4761ae3e7ebaa82ca30cf716f5388211b728709
Ram Mehta <ram.mehta@gmail.com>
2014-10-11 15:19:42 -0400

    Add start up script for production and add cluster to dependencies.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-module.exports = require('./lib/opensoc-ui');
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'production') {
+  module.exports = require('./lib/opensoc-ui');
+} else {
+  // Start cluster
+  var cluster = require('cluster');
+  var numCPUs = require('os').cpus().length;
+
+  if (cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+      cluster.fork();
+    }
+
+    cluster.on('exit', function (worker, code, signal) {
+      console.log('worker ' + worker.process.pid + ' died');
+    });
+  } else {
+    require('./lib/opensoc-ui');
+  }
+}
